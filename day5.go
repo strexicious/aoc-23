65dfe92c69b7a3acc1d221e19f72c737f629ca49
strexicious <strexicious@gmail.com>
2023-12-06 02:32:41 +0100

    Optimise part 2 with that initial plan
    This commit improves part to run in close to 1 ms whereas before it
    took around 7 minutes on my machine. The optmisation is no structural
    exploit, but a simple heuristic optmisation that I wanted to implement
    in the first place.
    
    Basically, instead of brute forcing all seed ranges by mapping all
    possible seeds one-by-one, we could map the seed ranges themselves.
    The problem with this was that there might be multiple mappings for
    a single input seed range. This just means that a seed range may split
    up into different sub-ranges that can be mapped fully together. (I am
    ignoring the part the sub-ranges may even need to split further, as
    it can become a recursive explanation, but this case is treated in code,
    by splitting valid ranges until no more need to split.)
    
    The main issue was implementing this splitting requires care, which
    was annoying for a day5 challenge. For example, the last case in
    `Range.split(...)` was not there and it kept crashing on me (rightfully).

--- day5.go
+++ day5.go
@@ -14,6 +14,48 @@ type RangeRemap struct {
 	cnt uint
 }
 
+type Range struct {
+	// both inclusive
+	start, end uint
+}
+
+// Splits range `r` such that points overlapping with
+// `or` are extracted as a new range and remaining non-contiguous
+// pieces of `r` are also returned as new ranges along with the
+// extracted range. If no overlap, empty slice is returned.
+func (r Range) split(or Range) []Range {
+	if or.end < r.start || r.end < or.start {
+		return []Range{r}
+	}
+
+	if r.start < or.start && or.end < r.end {
+		return []Range{
+			Range{r.start, or.start - 1},
+			or, Range{or.end + 1, r.end},
+		}
+	}
+
+	if or.end < r.end {
+		return []Range{
+			Range{or.end + 1, r.end},
+			Range{r.start, or.end},
+		}
+	}
+
+	if or.start > r.start {
+		return []Range{
+			Range{r.start, or.start - 1},
+			Range{or.start, r.end},
+		}
+	}
+
+	if or.start <= r.start && r.end <= or.end {
+		return []Range{r}
+	}
+
+	panic("should not reach?")
+}
+
 func (rr RangeRemap) includes(num uint) (bool, uint) {
 	if rr.src <= num && num < rr.src+rr.cnt {
 		return true, rr.dst + (num - rr.src)
@@ -36,7 +78,6 @@ func part1(in string) string {
 	s.Scan() // skip white line
 
 	moreMaps := [TOTAL_MAPS][]RangeRemap{}
-	invMoreMaps := [TOTAL_MAPS][]RangeRemap{}
 
 	s.Scan() // skip header
 	for mapIdx := 0; s.Scan(); {
@@ -53,7 +94,6 @@ func part1(in string) string {
 		cnt, _ := strconv.Atoi(lsplit[2])
 
 		moreMaps[mapIdx] = append(moreMaps[mapIdx], RangeRemap{uint(src), uint(dst), uint(cnt)})
-		invMoreMaps[mapIdx] = append(invMoreMaps[mapIdx], RangeRemap{dst: uint(src), src: uint(dst), cnt: uint(cnt)})
 	}
 
 	var minLoc uint = 1e9 // don't do this in real code
@@ -81,15 +121,15 @@ func part2(in string) string {
 	rdr := strings.NewReader(in)
 	s := bufio.NewScanner(rdr)
 	s.Scan()
-	seeds := []uint{}
-	for _, seedid := range strings.Split(s.Text()[7:], " ") {
-		seedid, _ := strconv.Atoi(seedid)
-		seeds = append(seeds, uint(seedid))
+	seedRanges := []Range{}
+	for seedSplit, i := strings.Split(s.Text()[7:], " "), 0; i < len(seedSplit); i += 2 {
+		start, _ := strconv.Atoi(seedSplit[i])
+		count, _ := strconv.Atoi(seedSplit[i+1])
+		seedRanges = append(seedRanges, Range{uint(start), uint(start) + uint(count)})
 	}
 	s.Scan() // skip white line
 
 	moreMaps := [TOTAL_MAPS][]RangeRemap{}
-	invMoreMaps := [TOTAL_MAPS][]RangeRemap{}
 
 	s.Scan() // skip header
 	for mapIdx := 0; s.Scan(); {
@@ -106,27 +146,50 @@ func part2(in string) string {
 		cnt, _ := strconv.Atoi(lsplit[2])
 
 		moreMaps[mapIdx] = append(moreMaps[mapIdx], RangeRemap{uint(src), uint(dst), uint(cnt)})
-		invMoreMaps[mapIdx] = append(invMoreMaps[mapIdx], RangeRemap{dst: uint(src), src: uint(dst), cnt: uint(cnt)})
+	}
+
+	for level := 0; level < TOTAL_MAPS; level += 1 {
+		for _, rr := range moreMaps[level] {
+			for i := 0; i < len(seedRanges); i += 1 {
+				cr := seedRanges[i]
+				newRanges := cr.split(Range{start: rr.src, end: rr.src + rr.cnt - 1})
+				if len(newRanges) == 1 {
+					continue
+				}
+
+				// we will mark cr as invalid by setting
+				// end = 0 and start = 1, so that later
+				// these can be filtered out
+				seedRanges[i] = Range{1, 0}
+
+				for _, nr := range newRanges {
+					seedRanges = append(seedRanges, nr)
+				}
+			}
+		}
+
+		for i, r := range seedRanges {
+			if r.end == 0 && r.start == 1 {
+				continue
+			}
+
+			for _, rr := range moreMaps[level] {
+				if ok, dst := rr.includes(r.start); ok {
+					seedRanges[i] = Range{dst, dst + (r.end - r.start)}
+					break
+				}
+			}
+		}
 	}
 
 	var minLoc uint = 1e9 // don't do this in real code
-	for i := 0; i < len(seeds); i += 2 {
-		fmt.Println("Seed group:", i>>1)
-		for seedid := seeds[i]; seedid < seeds[i]+seeds[i+1]; seedid += 1 {
-			k := seedid
+	for _, r := range seedRanges {
+		if r.end == 0 && r.start == 1 {
+			continue
+		}
 
-			for _, remaps := range moreMaps {
-				for _, rr := range remaps {
-					if ok, mapK := rr.includes(k); ok {
-						k = mapK
-						break
-					}
-				}
-			}
-
-			if k < minLoc {
-				minLoc = k
-			}
+		if r.start < minLoc {
+			minLoc = r.start
 		}
 	}
 
